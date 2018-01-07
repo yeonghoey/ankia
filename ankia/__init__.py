9e1770975103f8f062bcb16193ac9788d5e9b26d
Yeongho Kim <yeonghoey@gmail.com>
2018-01-08 03:39:50 +0900

    Cleanup project

--- ankia/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for ankia."""
-
-__author__ = """Yeongho Kim"""
-__email__ = 'yeonghoey@gmail.com'
-__version__ = '0.1.0'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use ankia in a project::
-
-    import ankia
